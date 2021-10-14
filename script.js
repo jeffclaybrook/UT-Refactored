654056eef87e47e3f80636b4229629c1e1ec1151
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2021-10-14 15:00:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ menuBtn.addEventListener("click", function() {
    document.querySelector("nav").classList.toggle("expanded");
 })
 
+let header = document.querySelector("header");
+var scrollPrev = window.pageYOffset;
+window.onscroll = function () {
+    var scrollCur = window.pageYOffset;
+    if (scrollPrev > scrollCur) {
+        header.style.top = "0";
+    } else {
+        header.style.top = "-72px";
+
+    }
+    scrollPrev = scrollCur;
+}
+
 let navLink = document.querySelectorAll("nav a");
 for (let i=0; i<navLink.length; i++) {
    navLink[i].onclick = function() {
